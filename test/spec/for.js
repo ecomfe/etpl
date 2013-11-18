73a84d649bb1a34aebefa4f219e11860b484fb41
erik <erik168@163.com>
2013-11-18 15:29:57 +0800

    modify test case

--- test/spec/for.js
+++ test/spec/for.js
@@ -5,7 +5,9 @@ define(
         var text = readText( 'spec/for.text.html' );
         var data = {
             myList: eval(text.myList),
-            persons: eval(text.persons)
+            myObj: eval(text.myObj),
+            persons: eval(text.persons),
+            personsIndex: eval(text.personsIndex)
         };
         etpl.compile( text.tpl );
 
@@ -23,6 +25,34 @@ define(
             it('can use property accessor', function() {
                 var renderer = etpl.getRenderer('forItemPropertyAccessTarget');
                 expect(renderer(data)).toEqual(text['expect-forItemPropertyAccessTarget']);
+            });
+        });
+
+        describe('Object traversal', function() {
+            it('can read "item" variable', function() {
+                var renderer = etpl.getRenderer('forOItemTarget');
+                expect(renderer(data)).toEqual(text['expect-forOItemTarget']);
+            });
+
+            it('can read "item" and "key" variables', function() {
+                var renderer = etpl.getRenderer('forOItemKeyTarget');
+                expect(renderer(data)).toEqual(text['expect-forOItemKeyTarget']);
+            });
+
+            it('can use property accessor', function() {
+                var renderer = etpl.getRenderer('forOItemPropertyAccessTarget');
+                expect(renderer(data)).toEqual(text['expect-forOItemPropertyAccessTarget']);
+            });
+        });
+
+        describe('Traversal', function() {
+            it('can be nested', function() {
+                var renderer = etpl.getRenderer('forNestedTarget');
+                var result = renderer(data);console.log(result)
+                expect(result.indexOf(text['except-nested-1'])).toBeGreaterThan(-1);
+                expect(result.indexOf(text['except-nested-2'])).toBeGreaterThan(-1);
+                expect(result.indexOf(text['except-nested-3'])).toBeGreaterThan(-1);
+                expect(result.indexOf(text['except-nested-4'])).toBeGreaterThan(-1);
             });
         });
     }
