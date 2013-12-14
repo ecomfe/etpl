0f2a9de6939ccdcb05a34a0639c286fee093999d
erik <erik168@163.com>
2013-12-14 11:40:25 +0800

    add try navigator

--- try.js
+++ try.js
@@ -3,28 +3,8 @@
     var dataText = document.getElementById( 'text-data' );
     var resultText = document.getElementById( 'text-result' );
 
-    tplText.value = [
-        '<ul>',
-        '<!-- for: ${persons} as ${person} -->',
-        '    <li>${person.name}[${person.email}]</li>',
-        '<!-- /for -->',
-        '</ul>'
-    ].join( '\n' );
-
-    dataText.value = [
-        '{',
-        '  persons: [',
-        '    {',
-        '      name:"erik", ',
-        '      email:"errorrik@gmail.com"',
-        '    },',
-        '    {',
-        '      name:"otakustay",',
-        '      email:"otakustay@gmail.com"',
-        '    }',
-        '  ]',
-        '}'
-    ].join( '\n' );
+    tplText.value = 'Hello ${name}!';
+    dataText.value = '{ "name": "ETPL" }';
 
     function tryRender() {
         var tpl = tplText.value;
@@ -66,4 +46,137 @@
 
     tryRender();
     tplText.onblur = dataText.onblur = tryRender;
+
+    var tryCodes = {
+        'if': {
+            template: [
+                '<!-- if: ${num} === ${str} -->',
+                'same type and value',
+                '<!-- elif: ${num} == ${str} -->',
+                'different type same value',
+                '<!-- else -->',
+                'different value',
+                '<!--/if-->'
+            ].join( '\n' ),
+            data: [
+                '{',
+                '  num: 1,',
+                '  str: "1"',
+                '}'
+            ].join( '\n' )
+        },
+        'for': {
+            template: [
+                '<ul>',
+                '<!-- for: ${persons} as ${person} -->',
+                '    <li>${person.name}[${person.email}]</li>',
+                '<!-- /for -->',
+                '</ul>'
+            ].join( '\n' ),
+            data: [
+                '{',
+                '  persons: [',
+                '    {',
+                '      name:"erik", ',
+                '      email:"errorrik@gmail.com"',
+                '    },',
+                '    {',
+                '      name:"otakustay",',
+                '      email:"otakustay@gmail.com"',
+                '    }',
+                '  ]',
+                '}'
+            ].join( '\n' )
+        },
+        'use': {
+            template: [
+                '<ul>',
+                '<!-- for: ${persons} as ${p} -->',
+                '  <!-- use: item(main=${p.name}, sub=${p.email}) -->',
+                '<!-- /for -->',
+                '</ul>',
+                '<!-- target: item --><li>${main}[${sub}]</li>'
+            ].join( '\n' ),
+            data: [
+                '{',
+                '  persons: [',
+                '    {',
+                '      name:"erik", ',
+                '      email:"errorrik@gmail.com"',
+                '    },',
+                '    {',
+                '      name:"otakustay",',
+                '      email:"otakustay@gmail.com"',
+                '    }',
+                '  ]',
+                '}'
+            ].join( '\n' )
+        },
+        'import': {
+            template: [
+                '<p><!-- import: hello --></p>',
+                '<!-- target: hello -->Hello, I am ${name}!'
+            ].join( '\n' ),
+            data: '{ "name": "ETPL" }'
+        },
+        'filter': {
+            template: [
+                '<!-- filter: html -->',
+                'Hello, I am <b>${name}</b>!',
+                '<!-- /filter -->',
+                'Hello, I am <b>${name}</b>!'
+            ].join( '\n' ),
+            data: '{ "name": "ETPL" }'
+        },
+        'master': {
+            template: [
+                '<!-- master: myMaster -->',
+                '<header><!-- contentplaceholder: head -->header<!-- /contentplaceholder--></header>',
+                '<p><!-- contentplaceholder: body -->body<!-- /contentplaceholder--></p>',
+                '<footer><!-- contentplaceholder: foot --></footer>',
+                '',
+                '<!-- target: myTarget(master=myMaster) -->',
+                '<!-- content: body -->Hello, I am ${name}!<!-- /content -->',
+                '<!-- content: foot --><!-- import: copyright --><!-- /content -->',
+                '',
+                '<!-- target: copyright -->&copy;2013'
+            ].join( '\n' ),
+            data: '{ "name": "ETPL" }'
+        },
+        'vs': {
+            template: [
+                'Hello ${htmlcontent}!',
+                'Hello ${htmlcontent | raw}!',
+                'Search <a href="search?${urlcontent | url}">${urlcontent}</a>!'
+            ].join( '\n' ),
+            data: [
+                '{ ',
+                '  "htmlcontent": "<b>ETPL</b>",',
+                '  "urlcontent": "中文"',
+                '}'
+            ].join( '\n' )
+        },
+        'var': {
+            template: [
+                '<!-- var: name = "errorrik" -->',
+                'Hello ${name}!'
+            ].join( '\n' ),
+            data: '{ "name": "ETPL" }'
+        }
+    };
+
+    document.getElementById( 'try-nav' ).onclick = function ( e ) {
+        e = e || window.event;
+        var target = e.srcElement || e.target;
+
+        if ( target.tagName == 'LI' ) {
+            var type = target.getAttribute( 'data-nav' ) || target.innerHTML;
+            var tryCode = tryCodes[ type ];
+            if ( tryCode ) {
+                tplText.value = tryCode.template;
+                dataText.value = tryCode.data;
+                tryRender();
+            }
+        }
+    };
 })();
