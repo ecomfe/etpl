7143adb5ee6c2b89073a2c5c77d92c1a902dff00
erik <erik168@163.com>
2014-07-21 14:36:39 +0800

    refactor and add #31 cases

--- test/spec/import.spec.js
+++ test/spec/import.spec.js
@@ -8,23 +8,36 @@
 
     describe('Import', function() {
         it('can be used in target', function() {
-            var renderer = etpl.getRenderer('importPara');
-            expect(renderer(data)).toEqual(text['expect-importPara']);
+            var renderer = etpl.compile(text['tpl-para']);
+            expect(renderer(data)).toEqual(text['expect-para']);
         });
 
-        it('can be used in master applyed, both content and contentplaceholder', function() {
-            var renderer = etpl.getRenderer('importMasterTarget');
-            expect(renderer(data)).toEqual(text['expect-importMasterTarget']);
+        it('can be used in master applyed', function() {
+            var renderer = etpl.compile(text['tpl-master']);
+            expect(renderer(data)).toEqual(text['expect-master']);
         });
 
         it('When dependence unready, getRenderer should return null. Renderer can be getted when dependence ready.', function() {
-            var renderer = etpl.getRenderer('importMasterTarget2');
+            var renderer = etpl.compile(text['tpl-unready-begin']);
             expect(renderer).toBeNull();
 
-            etpl.compile( text['other-imported'] );
-            renderer = etpl.getRenderer('importMasterTarget2');
+            etpl.compile(text['tpl-unready-then']);
+            renderer = etpl.getRenderer('importUnready');
             expect(typeof renderer).toBe('function');
-            expect(renderer(data)).toBe(text['expect-importMasterTarget2']);
+            expect(renderer(data)).toBe(text['expect-unready']);
+        });
+
+        it('can override block which declared in target', function() {
+            var renderer = etpl.compile(text['tpl-block']);
+            expect(renderer()).toEqual(text['expect-block']);
+        });
+
+        it('and override block, the own target can be extended', function() {
+            var renderer = etpl.compile(text['tpl-mix']);
+            expect(renderer()).toEqual(text['expect-mix-target']);
+
+            renderer = etpl.getRenderer('importMixMaster');
+            expect(renderer()).toEqual(text['expect-mix-master']);
         });
     });
 
