fe98a6861ff44eca5e0cedce68bc0072360d0db9
erik <erik168@163.com>
2014-07-21 19:35:20 +0800

    follow efe js code style

--- src/tpl.js
+++ src/tpl.js
@@ -1,33 +1,34 @@
 /**
  * ETPL (Enterprise Template)
  * Copyright 2013 Baidu Inc. All rights reserved.
- * 
+ *
  * @file 加载模板的amd模块
  * @author errorrik(errorrik@gmail.com)
  */
 
-define( 
-    function ( require, exports, module ) {
-        var etpl = require( '.' );
+define(
+    function (require, exports, module) {
+        var etpl = require('.');
 
         return {
-            load: function ( resourceId, req, load, config ) {
-                var xhr = window.XMLHttpRequest 
+            load: function (resourceId, req, load) {
+                var xhr = window.XMLHttpRequest
                     ? new XMLHttpRequest()
-                    : new ActiveXObject( 'Microsoft.XMLHTTP' );
+                    : new ActiveXObject('Microsoft.XMLHTTP');
+
                 xhr.open( 'GET', req.toUrl( resourceId ), true );
                 xhr.onreadystatechange = function () {
-                    if ( xhr.readyState == 4 ) {
-                        if ( xhr.status >= 200 && xhr.status < 300 ) {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status >= 200 && xhr.status < 300) {
                             var source = xhr.responseText;
                             var moduleConfig = module.config();
-                            if ( typeof moduleConfig.autoCompile == 'undefined'
-                                || moduleConfig.autoCompile 
+                            if (moduleConfig.autoCompile
+                                || moduleConfig.autoCompile == null
                             ) {
-                                etpl.compile( source );
+                                etpl.compile(source);
                             }
 
-                            load( source );
+                            load(source);
                         }
 
                         xhr.onreadystatechange = new Function();
@@ -35,7 +36,7 @@ define(
                     }
                 };
 
-                xhr.send( null );
+                xhr.send(null);
             }
         };
     }
